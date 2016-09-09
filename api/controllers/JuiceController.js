13d7077d21860722e3a6abd8753bed9a33c20e0d
Stuart Filar <stuart.filar@gmail.com>
2016-09-09 16:31:56 -0400

    commit 2016-09-09, backend code working, need to finalize frontend

--- api/controllers/JuiceController.js
+++ api/controllers/JuiceController.js
@@ -16,23 +16,23 @@ module.exports = {
    */
   create: function (req, res) {
         
-        if(req.method != "POST"){
-          return res.view('create');
+    if(req.method != "POST"){
+      return res.view('create');
+    }
+
+    var args = {
+        data: req.body,
+        headers: { "Content-Type": "application/json" }
+    };
+     
+    client.post(endpoint, args, function (data, response) {
+        // return res.view('create', {success: { message: "Record added successfully"}});
+        if(response.statusCode != "201"){
+            return res.view('create', {error:{message: response.statusMessage + ": " + data.reason}});
         }
 
-        var args = {
-            data: req.body,
-            headers: { "Content-Type": "application/json" }
-        };
-         
-        client.post(endpoint, args, function (data, response) {
-            // return res.view('create', {success: { message: "Record added successfully"}});
-            if(response.statusCode != "201"){
-                return res.view('create', {error:{message: response.statusMessage + ": " + data.reason}});
-            }
-
-            return res.view('create', {success:{message: "Record created successfully"}});
-        })
+        return res.view('create', {success:{message: "Record created successfully"}});
+    })
   },
 
   /**
@@ -51,39 +51,58 @@ module.exports = {
    * `JuiceController.update()`
    */
   update: function (req, res) {
-    
+
     if(req.method != "POST"){
-      
       client.get(endpoint, function (data, response) {
         return res.view('update', {juices: data});
       }).on('error', function (err) {
         return res.view('update', {error: { message: "There was an error getting the record"}});
       });
+    } else {
 
-    }else{
       var args = {
         data: req.body,
-          headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json" }
       };
-         
-      client.put(endpoint, args, function (data, response) {
+        
+      var endpointUpdate = endpoint + "/" + req.body.id;
+
+      client.put(endpointUpdate, args, function (data, response) {
         // return res.view('create', {success: { message: "Record added successfully"}});
-        if(response.statusCode != "201"){
+        if(response.statusCode != "200"){
           return res.view('update', {error:{message: response.statusMessage + ": " + data.reason}});
         }
           return res.view('update', {success:{message: "Record updated successfully"}});
       })
-    }        
-  },
-
+    }
+  },        
 
   /**
    * `JuiceController.delete()`
    */
   delete: function (req, res) {
-    return res.json({
-      todo: 'delete() is not implemented yet!'
-    });
-  }
-};
 
+    if(req.method != "POST") {
+      client.get(endpoint, function (data, response) {
+        return res.view('delete', {juices: data});
+      }).on('error', function (err) {
+        return res.view('delete', {error: { message: "There was an error getting the record"}});
+      });
+    } else {
+      var args = {
+        data: req.body,
+        headers: { "Content-Type": "application/json" }
+      };
+           
+      var endpointDelete = endpoint + "/" + req.body.id;
+
+      client.delete(endpointDelete, args, function (data, response) {
+          // return res.view('create', {success: { message: "Record added successfully"}});
+        if(response.statusCode != "200"){
+          return res.view('delete', {error:{message: response.statusMessage + ": " + data.reason}});
+        }
+          return res.view('delete', {success:{message: "Record updated successfully"}});
+      })
+    }
+  }
+}
